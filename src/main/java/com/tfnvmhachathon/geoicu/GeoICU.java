1ee56ab44050fa66997a90a730f1855bf6a955a5
Thiago Veronese <thiago.veronese@visual-meta.com>
2016-01-21 13:00:02 +0100

    Spring Batch initial

---
+++ src/main/java/com/tfnvmhachathon/geoicu/GeoICU.java
@@ -0,0 +1,13 @@
+package com.tfnvmhachathon.geoicu;
+
+import org.springframework.boot.SpringApplication;
+
+public class GeoICU
+{
+
+	public static void main(String[] args)
+	{
+		System.exit(SpringApplication.exit(SpringApplication.run(BatchConfiguration.class, args)));
+	}
+
+}
